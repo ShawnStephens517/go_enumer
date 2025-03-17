cde1a28372e5f6a18f36040bc171da9e780b7ce1
Shawn Stephens <shawnstephens517@gmail.com>
2025-03-17 14:18:48 +0100

    Update embed_app.go

--- pkg/nefarious/windows_actions/embed_app.go
+++ pkg/nefarious/windows_actions/embed_app.go
@@ -1,5 +1,6 @@
 // +build windows
 
+//package windows_actions
 /*
 The intent of this action is to "Embed" a secondary Go binary into the memory of the Current App
 The idea is to be extra stealthy with our execution. Run additional "nefarious" actions from memory.
@@ -7,7 +8,7 @@ Should be used to help enhance Detection rules for these types of scenarios outs
 Atomic Red Team atomics (Powershell IEX/ IWR;IEX)
 
 TODO
-[] Write secondary application
+[] Write secondary application - Possibly use CTF_Serve. This is a simple Web Server that hosts the files in a specific Directory or could use default settings
 [] Review github.com/amenzhinsky/go-memexec
 [] Call the secondary binary at runtime to be executed similar to ATR powershell version (if possible)
 */
