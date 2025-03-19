69647aae0073296f3c56eeda57f3446d915faeab
Shawn Stephens <shawnstephens517@gmail.com>
2025-03-19 12:16:32 +0100

    Create receiver.go. Establish directory structure

---
+++ pkg/nefarious/linuxactions/receiver.go
@@ -0,0 +1,13 @@
+//go:build linux
+
+
+package linuxactions
+import(
+  "fmt"
+)
+
+func Receiver(){
+    fmt.Println("Starting Receiver...?")
+    fmt.Println("I receive commands to run and ship results back... Proxy me through AWS,GCP,Azure or other infra for effect?)
+    //Code Me Here
+}
