b2e6e708f294efc5fdfaf2c0be579d250ce41af6
Shawn Stephens <shawnstephens517@gmail.com>
2025-03-19 11:53:43 +0100

    Update windows_enhanced.go

--- pkg/nefarious/windows_enhanced.go
+++ pkg/nefarious/windows_enhanced.go
@@ -5,6 +5,7 @@ package nefarious
 
 import (
 	"fmt"
+	"github.com/shawnstephens517/go_enumer/pkg/nefarious/windowsactions"
 )
 
 /*
@@ -13,28 +14,10 @@ This will serve as the handle/middleware between the logic and the call
 at runtime.
 */
 
-func DLLInject() {
-	fmt.Println("Attempting an DLL Inject")
-	//Code Me Here
-}
-
-func ShellPopCalc() {
-	fmt.Println("Hooking the Windows API to pop a Calc Instance..\n Check your Logs!!!")
-	//Code me Here
-}
-
-func ProcessInject() {
-	fmt.Println("Attempt Process Injection")
-	//Code Here
-}
-
-func Voltorb() {
-	// Self-Removing function. Silly Pokemon Reference to Self-Destruct
-	fmt.Println("Removes itself...")
-	//Code Here
-}
-
-func NewDLL() {
-	fmt.Println("Creating New DLL")
-	//Code Here
+func Chernobyl() (string, error){
+	var result string
+	injectDll := windowsactions.DLLInject()
+	injectProcess := windowsactions.ProcessInject()
+	newDLL := windowsactions.NewDLL()
+	selfDestruct := windowsactions.Voltorb()
 }
